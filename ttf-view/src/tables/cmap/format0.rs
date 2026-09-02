3d7944082df866a9269bae6a76bb5ae6ca04d22f
Chasmical <kaz@chsm.dev>
2026-09-02 08:26:53 +0700

    Add 'cmap' format 2 subtable

--- ttf-view/src/tables/cmap/format0.rs
+++ ttf-view/src/tables/cmap/format0.rs
@@ -1,11 +1,11 @@
 use crate::tables::cmap::GlyphId;
 
 #[repr(C)]
-pub struct CmapSubtable0 {
+pub struct CmapSubtableFormat0Repr {
     pub glyph_id_array: [u8; 256],
 }
 
-impl CmapSubtable0 {
+impl CmapSubtableFormat0Repr {
     pub const fn map(&self, codepoint: u8) -> GlyphId {
         self.glyph_id_array[codepoint as usize].into()
     }
--- ttf-view/src/tables/cmap/format2.rs
+++ ttf-view/src/tables/cmap/format2.rs
@@ -1,4 +1,7 @@
-use crate::types::{int16, uint16};
+use crate::{
+    tables::cmap::GlyphId,
+    types::{int16, uint16},
+};
 
 #[repr(C)]
 #[non_exhaustive]
@@ -17,4 +20,36 @@ pub struct SubHeaderRepr {
     pub id_range_offset: uint16,
 }
 
-// TODO: 'cmap' subtable format 2
+impl CmapSubtableFormat2Repr {
+    pub fn sub_headers(&self) -> &[SubHeaderRepr] {
+        let max_offset = self.sub_header_keys.iter().max().unwrap().get();
+        let count = (max_offset / 8) + 1;
+        unsafe { std::slice::from_raw_parts(self.sub_headers.as_ptr(), count as _) }
+    }
+    pub const fn sub_header(&self, high_byte: u8) -> &SubHeaderRepr {
+        let offset = self.sub_header_keys[high_byte as usize].get();
+        unsafe { &*self.sub_headers.as_ptr().byte_add(offset as usize) }
+    }
+
+    pub const fn map_one(&self, single_byte: u8) -> GlyphId {
+        let sub_header_zero = unsafe { &*self.sub_headers.as_ptr() };
+        sub_header_zero.map(single_byte)
+    }
+    pub const fn map_two(&self, high_byte: u8, low_byte: u8) -> GlyphId {
+        self.sub_header(high_byte).map(low_byte)
+    }
+}
+
+impl SubHeaderRepr {
+    pub const fn map(&self, low_byte: u8) -> GlyphId {
+        let offset = (low_byte as u16).checked_sub(self.first_code.get())?;
+        if offset > self.entry_count.get() {
+            return GlyphId::NOTDEF;
+        }
+
+        let result = unsafe { &*std::ptr::from_ref(&self.id_range_offset).add(offset as _) }.get();
+        let id_delta = if result == 0 { 0 } else { self.id_delta.get() as u16 };
+
+        GlyphId::new(result.wrapping_add(id_delta))
+    }
+}
