f06dbb4c46b207c342cf91d23c924b467b217d73
Chasmical <kaz@chsm.dev>
2026-09-06 13:25:07 +0700

    fix(types): fix Fixed/F2DOT14 bounds validation
    - Fix MIN and MAX bounds calculation (previous values were 2x the correct value).
    - Rust converts f32/f64 to i32/i64 in a saturating way, so no need to worry about overflow in new().
    - Add more tests covering values around MIN/MAX boundaries.
    - Expose STEP, MIN and MAX constants (typed as Fixed/F2DOT14).
    - Remove unnecessary get() in assert_eq!() in doc-tests.

--- ttf-view/src/types/fixed_point.rs
+++ ttf-view/src/types/fixed_point.rs
@@ -7,7 +7,11 @@ macro_rules! impl_fixed_point_number {
             $int:ty as [u8; $bytes:literal];
             $integer_bits:literal | $fraction_bits:literal as $fp:ty
         );
-        docs { DENOM = $d_denom:literal }
+        DENOM = $d_denom:literal;
+        STEP = $d_step:literal;
+        MIN = $d_min:literal;
+        MAX = $d_max:literal;
+        PRECISION = $d_precision:literal;
     ) => {
         #[doc = concat!("The [OpenType ", stringify!($Name), "][spec] type, a ")]
         #[doc = concat!(stringify!($integer_bits), ".", stringify!($fraction_bits), "-bit")]
@@ -26,9 +30,49 @@ macro_rules! impl_fixed_point_number {
         };
 
         impl $Name {
-            const STEP: $fp = 1.0 / (1 << $fraction_bits) as $fp;
-            const MIN: $fp = -(1 << $integer_bits) as $fp;
-            const MAX: $fp = ((1 << $integer_bits) - 1) as $fp;
+            const F_STEP: $fp = 1.0 / (1 << $fraction_bits) as $fp;
+            const F_MIN: $fp = -(1 << ($integer_bits - 1)) as $fp;
+            const F_MAX_EXCLUSIVE: $fp = (1 << ($integer_bits - 1)) as $fp;
+            const F_MAX: $fp = Self::F_MAX_EXCLUSIVE - Self::F_STEP;
+
+            #[doc = concat!("The difference between adjacent [`", stringify!($Name), "`] values.")]
+            ///
+            /// # Examples
+            ///
+            /// ```
+            #[doc = concat!("# use ttf_view::types::", stringify!($Name), ";")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::STEP, ", stringify!($d_step), ");")]
+            /// ```
+            pub const STEP: Self = Self::new(Self::F_STEP).unwrap();
+
+            /// The smallest value that can be represented by this type.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            #[doc = concat!("# use ttf_view::types::", stringify!($Name), ";")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::MIN, ", stringify!($d_min), ");")]
+            /// ```
+            pub const MIN: Self = Self::new(Self::F_MIN).unwrap();
+            /// The largest value that can be represented by this type.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            #[doc = concat!("# use ttf_view::types::", stringify!($Name), ";")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::MAX, ", stringify!($d_max), ");")]
+            /// ```
+            pub const MAX: Self = Self::new(Self::F_MAX).unwrap();
+
+            /// The amount of decimal places the type can accurately represent.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            #[doc = concat!("# use ttf_view::types::", stringify!($Name), ";")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::PRECISION, ", stringify!($d_precision), ");")]
+            /// ```
+            pub const PRECISION: u32 = (Self::F_STEP.recip() as u32).ilog10();
 
             #[doc = concat!("Creates a [`", stringify!($Name), "`] from [`", stringify!($fp), "`].")]
             ///
@@ -37,18 +81,17 @@ macro_rules! impl_fixed_point_number {
             /// ```
             #[doc = concat!("use ttf_view::types::", stringify!($Name), ";")]
             ///
-            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(-0.125).unwrap().get(), -0.125);")]
-            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(1.99609375).unwrap().get(), 1.99609375);")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(-0.125).unwrap(), -0.125);")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(1.99609375).unwrap(), 1.99609375);")]
             #[doc = concat!("assert_eq!(", stringify!($Name), "::new(123456.78), None);")]
             ///
             #[doc = concat!("// numbers are rounded towards the closest value representable by ", stringify!($Name))]
-            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156247).unwrap().get(), 0.515625);")]
-            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156250).unwrap().get(), 0.515625);")]
-            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156254).unwrap().get(), 0.515625);")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156247).unwrap(), 0.515625);")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156250).unwrap(), 0.515625);")]
+            #[doc = concat!("assert_eq!(", stringify!($Name), "::new(0.5156254).unwrap(), 0.515625);")]
             /// ```
             pub const fn new(num: $fp) -> Option<Self> {
-                if matches!(num, Self::MIN..Self::MAX) {
-                    // TODO: Could this sometimes result in overflow? e.g. 1.999999 wrapping to -2?
+                if matches!(num, Self::F_MIN..Self::F_MAX_EXCLUSIVE) {
                     Some(unsafe { Self::new_unchecked(num) })
                 } else {
                     None
@@ -58,8 +101,11 @@ macro_rules! impl_fixed_point_number {
             ///
             /// TODO: # Examples
             pub const unsafe fn new_unchecked(num: $fp) -> Self {
-                debug_assert!(matches!(num, Self::MIN..Self::MAX));
-                Self(((num / Self::STEP).round() as $int).to_be_bytes())
+                debug_assert!(matches!(num, Self::F_MIN..Self::F_MAX_EXCLUSIVE));
+
+                // Note: No need to worry about 1.9999999 wrapping to -2, because `as` here
+                // converts in a saturating way (even Infinity becomes 0xFFFF through `as`).
+                Self(((num / Self::F_STEP).round() as $int).to_be_bytes())
             }
 
             #[doc = concat!("Creates a [`", stringify!($Name), "`] from big-endian bytes.")]
@@ -75,9 +121,6 @@ macro_rules! impl_fixed_point_number {
                 self.0
             }
 
-            /// The amount of decimal places the type can accurately represent.
-            pub const PRECISION: u32 = (Self::STEP.recip() as u32).ilog10();
-
             #[doc = concat!("Returns this [`", stringify!($Name), "`] fraction's numerator")]
             #[doc = concat!("(`", stringify!($Name), "` represented as <math><mfrac><mi>numerator</mi><mn>", stringify!($d_denom), "</mn></mfrac></math>).")]
             ///
@@ -89,7 +132,7 @@ macro_rules! impl_fixed_point_number {
             ///
             /// TODO: # Examples
             pub const fn get(&self) -> $fp {
-                self.frac_num() as $fp * Self::STEP
+                self.frac_num() as $fp * Self::F_STEP
             }
             #[doc = concat!("Rounds this [`", stringify!($Name), "`]'s value to [`PRECISION`][Self::PRECISION] decimal places.")]
             ///
@@ -147,13 +190,22 @@ macro_rules! impl_fixed_point_number {
     }
 }
 
+// The constants specified here are re-calculated in the macro and then validated in doc-tests.
 impl_fixed_point_number! {
     pub struct Fixed(i32 as [u8; 4]; 16|16 as f64);
-    docs { DENOM = 65536 }
+    DENOM = 65536;
+    STEP = 0.0000152587890625;
+    MIN = -32768.0;
+    MAX = 32767.99998474121;
+    PRECISION = 4;
 }
 impl_fixed_point_number! {
     pub struct F2DOT14(i16 as [u8; 2]; 2|14 as f32);
-    docs { DENOM = 16384 }
+    DENOM = 16384;
+    STEP = 0.000061035156;
+    MIN = -2.0;
+    MAX = 1.999939;
+    PRECISION = 4;
 }
 
 #[cfg(test)]
@@ -162,9 +214,7 @@ mod tests {
 
     #[test]
     fn precision() {
-        assert_eq!(Fixed::PRECISION, 4);
-        assert_eq!(F2DOT14::PRECISION, 4);
-
+        // Precision tests for Fixed (font_revision)
         let f = Fixed::from_be_bytes(0x0001999A_u32.to_be_bytes());
         assert_eq!(format!("{}", f), "1.600006103515625");
         assert_eq!(format!("{}", f.round_to_precision()), "1.6");
@@ -173,10 +223,28 @@ mod tests {
         assert_eq!(format!("{:.1}", f), "1.6");
         assert_eq!(format!("{:.3}", f), "1.600");
         assert_eq!(format!("{:.7}", f), "1.6000061");
+
+        // Ensure the upper boundary is correctly rounded down
+        assert_eq!(Fixed::new(32767.999999999996).unwrap().to_be_bytes(), [0x7F, 0xFF, 0xFF, 0xFF]);
+        assert_eq!(Fixed::new(32768.0), None);
+        assert_eq!(F2DOT14::new(1.9999999).unwrap().to_be_bytes(), [0x7F, 0xFF]);
+        assert_eq!(F2DOT14::new(2.0), None);
+
+        // Ensure the lower boundary is at an integer
+        assert_eq!(Fixed::new(-32768.0).unwrap().to_be_bytes(), [0x80, 0x00, 0x00, 0x00]);
+        assert_eq!(Fixed::new(-32768.00000000001), None);
+        assert_eq!(F2DOT14::new(-2.0).unwrap().to_be_bytes(), [0x80, 0x00]);
+        assert_eq!(F2DOT14::new(-2.0000002), None);
     }
 
     #[test]
     fn fixed() {
+        // Check f64 bounds used in parameter validation
+        assert_eq!(Fixed::F_MIN, -32768.0);
+        assert_eq!(Fixed::F_MAX_EXCLUSIVE, 32768.0);
+        assert_eq!(Fixed::F_MAX, 32767.99998474121);
+
+        // Test a bunch of sample numbers
         let nums: [(u32, f64); _] = [
             (0x7FFF_FFFF, 32767.999985),
             (0x7FFF_FF00, 32767.996094),
@@ -198,12 +266,18 @@ mod tests {
 
             let real = Fixed::new(fp).unwrap().get();
             let diff = (real - fp).abs();
-            assert!(diff <= 0.1 * Fixed::STEP, "{real} != {fp} (Δ={diff})");
+            assert!(diff <= 0.1 * Fixed::F_STEP, "{real} != {fp} (Δ={diff})");
         }
     }
 
     #[test]
     fn f2dot14() {
+        // Check f32 bounds used in parameter validation
+        assert_eq!(F2DOT14::F_MIN, -2.0);
+        assert_eq!(F2DOT14::F_MAX_EXCLUSIVE, 2.0);
+        assert_eq!(F2DOT14::F_MAX, 1.999939);
+
+        // Test a bunch of sample numbers
         let nums: [(u16, f32); _] = [
             (0x7FFF, 1.999939),
             (0x7000, 1.750000),
@@ -223,7 +297,7 @@ mod tests {
 
             let real = F2DOT14::new(fp).unwrap().get();
             let diff = (real - fp).abs();
-            assert!(diff <= 0.1 * F2DOT14::STEP, "{real} != {fp} (Δ={diff})");
+            assert!(diff <= 0.1 * F2DOT14::F_STEP, "{real} != {fp} (Δ={diff})");
         }
     }
 }
